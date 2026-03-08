e4cac34bab8b3e16760cc3f46a80f91ef80ecc7d
Tahar0061 <82381299+Tahar0061@users.noreply.github.com>
2026-03-08 15:04:39 +0100

    Update mysite.py

--- mysite.py
+++ mysite.py
@@ -1,9 +1,3 @@
-# الحصول على الرد من جوجل
-        response = model.generate_content(user_text)
-        bot_reply = response.text
-        st.success(f"🤖 المساعد: {bot_reply}")
-        
-        # نطق الرد
-        tts = gTTS(text=bot_reply, lang='ar')
-        # ... باقي الكود
+باقي الكود
+
 
