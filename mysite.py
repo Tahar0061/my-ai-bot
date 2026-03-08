da1cac6854614c996f09a293e7a7203ed1bb19ab
Tahar0061 <82381299+Tahar0061@users.noreply.github.com>
2026-03-08 01:26:42 +0100

    Update mysite.py

--- mysite.py
+++ mysite.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-# ربط الكود بالمفتاح السري الذي وضعته في الإعدادات
+# جلب المفتاح السري الذي وضعته في الإعدادات
 api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
@@ -14,13 +14,14 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("اسألني أي شيء..."):
+if prompt := st.chat_input("كيف يمكنني مساعدتك؟"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+    
     with st.chat_message("assistant"):
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(prompt)
         st.markdown(response.text)
-        st.session_state.messages.append({"role": "assistant", "content": response.text})
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
