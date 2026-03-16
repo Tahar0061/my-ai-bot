c5fe441970eeb4058ffd2f52c0d5d7b97b2bded6
Tahar0061 <82381299+Tahar0061@users.noreply.github.com>
2026-03-16 01:10:47 +0100

    Update mysite.py

--- mysite.py
+++ mysite.py
@@ -418,7 +418,7 @@ if data:
         with col1:
             st.markdown(f"""
                 <div class="sun-card">
-                    <div class="metric-label" style="color: white; opacity: 0.9;">☀️شروق الشمس</div>
+                   st.markdown("<div>\u2600\ufe0f شروق الشمس</div>", unsafe_allow_html=True)
                     <div class="metric-value" style="color: white;">{datetime.fromisoformat(daily['sunrise'][0]).strftime('%I:%M %p')}</div>
                 </div>
             """, unsafe_allow_html=True)
